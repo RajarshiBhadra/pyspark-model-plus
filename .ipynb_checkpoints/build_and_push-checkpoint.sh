31ffbd57ef68b61a92399017e0efd35f1a800b97
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-01-24 14:33:11 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/build_and_push-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-python3 setup.py sdist bdist_wheel
-python3 -m pip install --user --upgrade twine
-python3 -m twine upload --repository pypi dist/*
-rm -f -r dist/ pyspark_model_plus.egg-info build/
-git config --global user.email bhadrarajarshi9@gmail.com
