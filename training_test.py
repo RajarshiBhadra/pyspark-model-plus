29af64ddeecd3c985496777e1b4a85676cca16f1
Florian Wolf <florian.2.wolf@uni-konstanz.de>
2021-12-21 11:38:50 +0100

    perfomance plot

--- training_test.py
+++ training_test.py
@@ -5,11 +5,12 @@ from pyspark.sql import SparkSession
 
 from pyspark_model_plus.evaluation import MulticlassLogLossEvaluator
 from pyspark_model_plus.training import StratifiedCrossValidator
-from pyspark_model_plus.transform import CustomMeanImputer
 from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.tuning import ParamGridBuilder
 from pyspark.ml import Pipeline
+
+import perfplot
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -28,11 +29,6 @@ df_test = spark.read.csv("iris_test.csv", inferSchema=True, header=True)
 
 stages = []
 indexer = StringIndexer(inputCol="Species", outputCol="labelIndex")
-stages += [indexer]
-# imputer = CustomMeanImputer(
-#     cols_to_impute=["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]
-# )
-# stages += [imputer]
 assembler = VectorAssembler(
     inputCols=["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"],
     outputCol="features",
@@ -42,7 +38,8 @@ stages += [assembler]
 pipeline = Pipeline(stages=stages)
 pipelineData = pipeline.fit(df_train)
 training_data = pipelineData.transform(df_train)
-print(training_data.explain(True))
+print(training_data.head(1))
+# print(training_data.explain(True))
 
 model = RandomForestClassifier(
     labelCol="labelIndex",
@@ -51,8 +48,11 @@ model = RandomForestClassifier(
     predictionCol="prediction",
 )
 paramGrid = ParamGridBuilder().addGrid(model.numTrees, [250, 252]).build()
-for parallel in range(1, 6):
-    cv = StratifiedCrossValidator(
+
+cv_dict = {}
+
+for parallel in range(1, 11):
+    cv_dict[parallel] = StratifiedCrossValidator(
         labelCol="labelIndex",
         estimator=model,
         estimatorParamMaps=paramGrid,
@@ -62,13 +62,22 @@ for parallel in range(1, 6):
         parallelism=parallel,
     )
 
-    # stratifiedCV = StratifiedCrossValidator(cv)
-    start_time = time.time()
-    cvModel = cv.fit(training_data)
-    print("parallel = {}, time = {}".format(parallel, time.time() - start_time))
+out = perfplot.bench(
+    setup=lambda n: training_data,
+    kernels=[lambda df: cv.fit(df) for cv in cv_dict.values()],
+    labels=[str(cv) for cv in cv_dict.keys()],
+    n_range=list(range(2)),  # how often for one method
+    xlabel="#loop",
+)
+out.show()
+out.save("perf.png", transparent=True, bbox_inches="tight")
+## stratifiedCV = StratifiedCrossValidator(cv)
+# start_time = time.time()
+# cvModel = cv.fit(training_data)
+# print("parallel = {}, time = {}".format(parallel, time.time() - start_time))
 
-test_data = pipelineData.transform(df_test)
-predictions = cvModel.transform(test_data)
-evaluator = MulticlassLogLossEvaluator(labelCol="labelIndex")
-accuracy = evaluator.evaluate(predictions)
-print(accuracy)
+# test_data = pipelineData.transform(df_test)
+# predictions = cvModel.transform(test_data)
+# evaluator = MulticlassLogLossEvaluator(labelCol="labelIndex")
+# accuracy = evaluator.evaluate(predictions)
+# print(accuracy)
